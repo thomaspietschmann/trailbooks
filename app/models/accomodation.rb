0566553a519c009285ec1b381a64fd6b44c9d739
Briz Le Lem <briz.lelem@gmail.com>
2020-11-24 12:43:51 +0100

    created relationships between models

--- app/models/accomodation.rb
+++ app/models/accomodation.rb
@@ -1,3 +1,4 @@
 class Accomodation < ApplicationRecord
   belongs_to :trail
+  has_many :reservations
 end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,7 @@
 class Itinerary < ApplicationRecord
   belongs_to :user
   belongs_to :trail
+  has_many :reservations
+  has_many :accomodations, through: :reservations
+  has_many :accomodations, through: :trails
 end
