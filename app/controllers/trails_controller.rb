19cf6433702825fd03b453b7f67adffaca7e578e
Thomas Pietschmann <thomas@pietschi.com>
2020-11-24 16:39:15 +0100

    some first seeds for iceland

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,12 +1,6 @@
 class TrailsController < ApplicationController
   def index
-    @accomodations = Accomodation.all
-
-    @markers = @accomodations.geocoded.map do |accomodation|
-      {
-        lat: accomodation.latitude,
-        lng: accomodation.longitude
-      }
+  
     end
   end
 end
