021ed9ea709732c56fcfd8e4093bbaacc4a3cf1e
Thomas Pietschmann <thomas@pietschi.com>
2020-11-25 13:23:37 +0100

    split trails show page in columns

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -4,12 +4,23 @@ class TrailsController < ApplicationController
     @accomodations = Accomodation.where(trail_id: params[:id])
 
     @markers = @accomodations.map do |accomodation|
+      icon = assign_icon(accomodation.category)
       {
         lat: accomodation.latitude,
         lng: accomodation.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { accomodation: accomodation }),
-        image_url: helpers.asset_url('mapbox-icon.png')
+        image_url: helpers.asset_url(icon)
       }
+    end
+  end
+
+  private
+
+  def assign_icon(category)
+    if category == "Hut"
+      image = 'mapbox-icon.png'
+    elsif category == "Campsite"
+      image = 'mapbox-icon2.png'
     end
   end
 end
