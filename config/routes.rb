c6404399a302fb1e07f74cbde27817d5f412a753
Leeanna Seidler <leeannaseidler@yahoo.com>
2020-11-25 15:58:04 +0100

    reservation update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :trails, only: [:index, :show]
   resources :accomodations, only: [:show]
-  resources :reservations, only: [:new, :create, :index]
+  resources :reservations, only: [:new, :create, :index, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
