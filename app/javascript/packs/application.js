807b673c191a5ab5184e8ef664fa06b30a497dc3
Briz Le Lem <briz.lelem@gmail.com>
2020-11-27 15:22:05 +0100

    fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -46,8 +46,13 @@ const addAccToIti = (accomodationContainer) => {
   const accName = accomodationContainer.querySelector("#accomodation-name").innerText;
   const accPrice = accomodationContainer.querySelector(".accomodation-show-pricing").innerText;
   const itinerary = document.querySelector("#itinerary");
-  const htmlCard = `<div class="accname">${accName}</div>
-                    <div class="accprice">${accPrice}</div>`
+  const htmlCard = `<div id="container">
+                      <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-geo-alt-fill" fill="#A31621" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                      </svg>
+                      <div class="accname"><b>${accName}</b></div>
+                      <div class="accprice">${accPrice}</div>
+                    </div>`
   addButton.addEventListener("click", event => {
     itinerary.insertAdjacentHTML("beforeend", htmlCard)
   })
