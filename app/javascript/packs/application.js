203463d570576356c908ec1137625f81b4030d74
Alizeh Khan <alizehedinburgh@gmail.com>
2020-11-29 16:48:24 +0100

    worked on the sidebar, map layout and started on itinerary cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -74,10 +74,7 @@ document.addEventListener('turbolinks:load', () => {
         const accName = infoWindow.querySelector(".acc-name").innerText;
         const accPrice = infoWindow.querySelector(".acc-price").innerText;
         const itinerary = document.querySelector("#itinerary");
-        const htmlCard = `<div id="container">
-                            <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-geo-alt-fill" fill="#A31621" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                            </svg>
+        const htmlCard = `<div id="itinerary-card">
                             <div class="accname"><b>${accName}</b></div>
                             <div class="accprice">${accPrice}</div>
                           </div>`
