5eeed5508778ad3e33b10126c412a63769c1256f
BrizLeLem <71378921+BrizLeLem@users.noreply.github.com>
2020-12-01 14:24:18 +0100

    Revert "Added a logic for ordering dates"

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
   def index
-    @reservations = Reservation.where(user: current_user).order(checkin_date: :asc)
+    @reservations = Reservation.where(user: current_user)
     respond_to do |format|
       format.html
       format.json { render json: { reservations: @reservations } }
